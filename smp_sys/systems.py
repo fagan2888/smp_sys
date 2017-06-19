6a65a01709b5ac0310cc4d7ad84f9d9452061411
Oswald Berthold <bertolos@informatik.hu-berlin.de>
2017-06-19 19:21:34 +0200

    added simplearm system from smq / explauto

--- smp_sys/systems.py
+++ smp_sys/systems.py
@@ -45,30 +45,56 @@ import numpy as np
 
 class SMPSys(object):
     def __init__(self, conf = {}):
+        self.conf = conf
+        # set_attr_from_dict(self, conf) # check that this is OK
+
+        # set_attr_from_dict_ifs(self, ifs_conf)
+        
         # self.id = self.__class__.__name__
         for k, v in conf.items():
             setattr(self, k, v)
             # print "%s.init self.%s = %s" % (self.__class__.__name__, k, v)
 
+        # ROS
+        if hasattr(self, 'ros') and self.ros:
+            # rospy.init_node(self.name)
+            self.subs = {}
+            self.pubs = {}
+            
     def step(self, x):
         return None
 
 class PointmassSys(SMPSys):
     """point mass system (pm)
 
-a pm is an abstract model of a rigid body robot represented by the coordinates 
+    a pm is an abstract model of a rigid body robot represented by the coordinates 
 
-taken from smq/robots.py, seems to be the same code as in
-explauto/environments/pointmass.py
-"""
+    taken from smq/robots.py, seems to be the same code as in explauto/environments/pointmass.py
+    """
+    defaults = {
+        'sysdim': 1,
+        'x0': np.random.uniform(-0.3, 0.3, (3, 1)),
+        'statedim': 3,
+        'dt': 1e-1,
+        'mass': 1.0,
+        "force_max":  1.0,
+        "force_min": -1.0,
+        "friction": 0.001,
+        "sysnoise": 1e-2,
+        }
+    
     def __init__(self, conf = {}):
         """Pointmass.__init__
 
-params:
- - mass: point _mass_
- - sysdim: dimension of system, usually 1,2, or 3D
- - order: control mode of the system, order = 0 kinematic, 1 velocity, 2 force
-"""
+        Arguments:
+        - conf: configuration dictionary
+        -- mass: point _mass_
+        -- sysdim: dimension of system, usually 1,2, or 3D
+        -- statedim: 1d pointmass has 3 variables (pos, vel, acc) in this model, so sysdim * 3
+        -- dt: integration time step
+        -- x0: initial state
+        -- order: NOT IMPLEMENT (control mode of the system, order = 0 kinematic, 1 velocity, 2 force)
+        """
         SMPSys.__init__(self, conf)
         
         # state is (pos, vel, acc).T
@@ -97,7 +123,7 @@ params:
 
     def apply_force(self, u):
         """control pointmass with force command (2nd order)"""
-        # print "u", u
+        # print "u", u, self.mass, u/self.mass
         # FIXME: insert motor transfer function
         a = (u/self.mass).reshape((self.sysdim, 1))
         # a += np.random.normal(0.05, 0.01, a.shape)
@@ -147,3 +173,218 @@ params:
     def compute_sensors(self):
         """compute the proprio and extero sensor values from state"""
         return self.x
+
+
+
+def forward(angles, lengths):
+    """ Link object as defined by the standard DH representation.
+
+    :param list angles: angles of each joint
+
+    :param list lengths: length of each segment
+
+    :returns: a tuple (x, y) of the end-effector position
+
+    .. warning:: angles and lengths should be the same size.
+    """
+    x, y = joint_positions(angles, lengths)
+    return x[-1], y[-1]
+
+def joint_positions(angles, lengths, unit='rad'):
+    """ Link object as defined by the standard DH representation.
+
+    :param list angles: angles of each joint
+
+    :param list lengths: length of each segment
+
+    :returns: x positions of each joint, y positions of each joints, except the first one wich is fixed at (0, 0)
+
+    .. warning:: angles and lengths should be the same size.
+    """
+    if len(angles) != len(lengths):
+        raise ValueError('angles and lengths must be the same size!')
+
+    if unit == 'rad':
+        a = np.array(angles)
+    elif unit == 'std':
+        a = np.pi * np.array(angles)
+    else:
+        raise NotImplementedError
+     
+    a = np.cumsum(a)
+    return np.cumsum(np.cos(a)*lengths), np.cumsum(np.sin(a)*lengths)
+
+class SimplearmSys(SMPSys):
+    """SimplearmSys
+
+    explauto's simplearm environment
+
+    an n-joint / n-1 segment generative robot
+    """
+
+    defaults = {
+        'sysdim': 1,
+        'x0': np.random.uniform(-0.3, 0.3, (3, 1)),
+        'statedim': 3,
+        'dt': 1e-1,
+        'mass': 1.0,
+        "force_max":  1.0,
+        "force_min": -1.0,
+        "friction": 0.001,
+        "sysnoise": 1e-2,
+        'dim_s_motor': 1,
+        'length_ratio': [1],
+        'm_mins': -1,
+        'm_maxs': 1,
+        'dim_s_extero': 2,
+        }
+    
+    def __init__(self, conf = {}):
+        """SimplearmSys.__init__
+
+        Arguments:
+        - conf: configuration dictionary
+        -- mass: point _mass_
+        -- sysdim: dimension of system, usually 1,2, or 3D
+        -- statedim: 1d pointmass has 3 variables (pos, vel, acc) in this model, so sysdim * 3
+        -- dt: integration time step
+        -- x0: initial state
+        -- order: NOT IMPLEMENT (control mode of the system, order = 0 kinematic, 1 velocity, 2 force)
+        """
+        SMPSys.__init__(self, conf)
+        
+        # state is (pos, vel, acc).T
+        # self.state_dim
+        if not hasattr(self, 'x0'):
+            self.x0 = np.zeros((self.statedim, 1))
+        self.x  = self.x0.copy()
+        self.cnt = 0
+
+
+
+        self.factor = 1.0
+
+        self.lengths = self.compute_lengths(self.dim_s_motor, self.length_ratio)
+
+        self.m = np.zeros((self.dim_s_motor, 1))
+        
+    def reset(self):
+        self.x = self.x0.copy()
+        
+    def step(self, x = None, world = None):
+        """update the robot, pointmass"""
+        # print "%s.step[%d] x = %s" % (self.__class__.__name__, self.cnt, x)
+        # x = self.inputs['u'][0]
+        self.apply_force(x)
+        # return dict of state values
+        return {'s_proprio': self.compute_sensors_proprio(),
+                's_extero':  self.compute_sensors_extero(),
+                's_all':     self.compute_sensors(),
+        }
+        
+
+    def compute_lengths(self, n_dofs, ratio):
+        l = np.ones(n_dofs)
+        for i in range(1, n_dofs):
+            l[i] = l[i-1] / ratio
+        return l / sum(l)
+
+    def compute_motor_command(self, m):
+        m *= self.factor
+        return np.clip(m, self.m_mins, self.m_maxs)
+
+    def compute_sensors_proprio(self):
+        return self.m
+
+    def step(self, x):
+        """update the robot, pointmass"""
+        print "%s.step x = %s" % (self.__class__.__name__, x)
+        # print "x", x.shape
+        self.m = self.compute_motor_command(self.m + x)# .reshape((self.dim_s_motor, 1))
+        
+        # print "m", m
+        # self.apply_force(x)
+        return {"s_proprio": self.m, # self.compute_sensors_proprio(),
+                "s_extero":  self.compute_sensors_extero(),
+                's_all':     self.compute_sensors(),
+                }
+
+    def compute_sensors_extero(self):
+        hand_pos = np.array(forward(self.m, self.lengths)).reshape((self.dim_s_extero, 1))
+        hand_pos += self.sysnoise * np.random.randn(*hand_pos.shape)
+        # print "hand_pos", hand_pos.shape
+        return hand_pos
+
+    def compute_sensors(self):
+        """compute the proprio and extero sensor values from state"""
+        return np.vstack((self.m, self.compute_sensors_extero()))
+        # return self.x
+    
+# class SimpleArmRobot(Robot2):
+#     def __init__(self, conf, ifs_conf):
+#         Robot2.__init__(self, conf, ifs_conf)
+        
+#         # self.length_ratio = length_ratio
+#         # self.noise = noise
+
+#         self.factor = 1.0
+
+#         self.lengths = self.compute_lengths(self.dim_s_motor, self.length_ratio)
+
+#         self.m = np.zeros((self.dim_s_motor, 1))
+
+#     def compute_lengths(self, n_dofs, ratio):
+#         l = np.ones(n_dofs)
+#         for i in range(1, n_dofs):
+#             l[i] = l[i-1] / ratio
+#         return l / sum(l)
+
+#     def compute_motor_command(self, m):
+#         m *= self.factor
+#         return np.clip(m, self.m_mins, self.m_maxs)
+
+#     def step(self, world, x):
+#         """update the robot, pointmass"""
+#         print "%s.step world = %s, x = %s" % (self.__class__.__name__, world, x)
+#         # print "x", x.shape
+#         self.m = self.compute_motor_command(self.m + x)# .reshape((self.dim_s_motor, 1))
+        
+#         # print "m", m
+#         # self.apply_force(x)
+#         return {"s_proprio": self.m, # self.compute_sensors_proprio(),
+#                 "s_extero": self.compute_sensors_extero()}
+
+#     def compute_sensors_extero(self):
+#         hand_pos = np.array(forward(self.m, self.lengths)).reshape((self.dim_s_extero, 1))
+#         hand_pos += self.sysnoise * np.random.randn(*hand_pos.shape)
+#         # print "hand_pos", hand_pos.shape
+#         return hand_pos
+                
+
+    
+sysclasses = [SimplearmSys, PointmassSys]
+
+
+if __name__ == "__main__":
+    """smp_sys.systems.main
+
+    simple test for this file's systems:
+    - iterate all known classes
+    - run system for 1000 steps on it's own proprioceptive (motor) sensors
+    - plot timeseries
+    """
+    for c in sysclasses:
+        print "class", c
+        c_ = c(conf = c.defaults)
+        c_data = []
+        for i in range(1000):
+            d = c_.step(x = c_.compute_sensors_proprio() * 0.98)['s_all'].copy()
+            # print "d", d
+            c_data.append(d)
+
+        # print c_data
+        # print np.array(c_data).shape
+
+        import matplotlib.pylab as plt
+        plt.plot(np.array(c_data)[...,0])
+        plt.show()
