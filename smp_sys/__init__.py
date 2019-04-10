9731da2ebd74549ab2a2c174e0be174a34107414
Oswald Berthold <oswald.berthold@gmail.com>
2019-04-10 15:58:28 +0200

    disabling systems_ros?

--- smp_sys/__init__.py
+++ smp_sys/__init__.py
@@ -2,10 +2,14 @@
 
 smp systems
 """
-__all__ = ['systems', 'systems_ros', 'systems_bhasim']
+__all__ = ['systems', 'systems_bhasim']
 
+import os
 from smp_sys import *
 
+# if os.
+
+# 'systems_ros'
 # import smp_sys.systems as systems
 # import smp_sys.systems_ros as systems_ros
 # import smp_sys.systems_bhasim as systems_bhasim
